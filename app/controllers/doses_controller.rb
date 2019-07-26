0e9222248156464e0b69b7b276db836b6903976b
Jinhong Wang <1320358947@qq.com>
2019-07-26 15:09:51 +0800

    completed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,17 @@
 class DosesController < ApplicationController
-before_action :list_cocktails
+before_action :find_cocktail
   def new
-    @cocktail = @cocktails.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "doses/new"
+    end
 
   end
 
@@ -15,11 +21,14 @@ before_action :list_cocktails
 
 
   private
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
   def list_cocktails
     @cocktails = Cocktail.all
   end
   def dose_params
-    params.require(:dose).permit(:description, :ingredient)
+    params.require(:dose).permit(:description, :ingredient_id)
 
   end
 end
